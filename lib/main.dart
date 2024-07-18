b0d6b110c33370475880951fe5f7f15ceb0c8103
Mercy Shee <thigamercy399@gmail.com>
2024-07-18 11:09:27 +0300

    feat: added a colour scheme to help theme texts, cards, buttons and the app in general

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColourScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp( MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        colorScheme: kColourScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColourScheme.onPrimaryContainer,
+          foregroundColor: kColourScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColourScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColourScheme.primaryContainer,
+            foregroundColor: kColourScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+                color: kColourScheme.onSecondaryContainer,
+              ),
+            ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
